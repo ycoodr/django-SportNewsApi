b7db2819055e9ee43c6a77c6f23a46b752efc190
ycoodr <ycoodr@gmail.com>
2022-07-01 11:25:17 +0300

    api и первые новости

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is my first url")
+    response = requests.get("https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=e2b88956dc67458fba2f0fdf463ecfe3")
+    jsonResponse = response.json()
+    articles = jsonResponse['articles']
+    return render(request, 'blog/index.html', {'articles': articles})
