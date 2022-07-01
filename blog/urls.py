5e6af7431cb3abf7f74544b5e00f6f21d28b5f0b
ycoodr <ycoodr@gmail.com>
2022-07-01 12:58:27 +0300

    конец

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('singleArticle', views.singleArticle, name="singleArticle"),
+    path('contact', views.contact, name="contact"),
+    path('about', views.about, name="about")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import requests
 
 # Create your views here.
@@ -8,3 +9,15 @@ def index(request):
     jsonResponse = response.json()
     articles = jsonResponse['articles']
     return render(request, 'blog/index.html', {'articles': articles})
+
+def singleArticle(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    image = request.POST.get('image')
+    return render(request, 'blog/single_article.html', {'title': title, 'content': content, 'image': image})
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
